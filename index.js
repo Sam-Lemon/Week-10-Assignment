1840046b9ab8f7e5a97cf9b05fb4d7c3efb6768f
Sam-Lemon <theatrewhisper@gmail.com>
2024-05-10 03:18:25 -0700

    basic level project done

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
+let id = 0;
+
+document.getElementById('add').addEventListener('click', () => {
+    let myTable = document.getElementById('table'); //brings the table over and creates a new variable
+    let row = myTable.insertRow(1); //header row is at position 0, so first inserted row needs to be 1
+    row.setAttribute('id', `item-${id}`); //this gives each row it's own id
+    row.insertCell(0).innerHTML = document.getElementById('new-title').value; //inserting the user inputted data into the first cell of the row
+    row.insertCell(1).innerHTML = document.getElementById('new-author').value //inserting the user inputted data into the second cell of the row
+    row.insertCell(2).innerHTML = document.getElementById('new-rating').value; //inserting the user inputted data into the third cell of the row
+    
+    // let newRating = row.insertCell(2);
+    // newRating.appendChild(reviewStars(num));
+
+    row.insertCell(3).innerHTML = document.getElementById('new-finish-date').value; //inserting the user inputted date into the fourth cell of the row
+    let remove = row.insertCell(4); //puts the button created by the function below into the fifth cell of the row
+    remove.appendChild(createDeleteButton(id++)); //attaching the function to create a delete button to the remove variable (which is inserted into it's appropriate cell)
+
+
+    document.getElementById('new-title').value = ''; //resets field to empty
+    document.getElementById('new-author').value = ''; //resets field to empty
+    document.getElementById('new-rating').value = ''; //resets field to empty
+    document.getElementById('new-finish-date').value = ''; //resets date field
+});
+
+function createDeleteButton(id) { //creating a button in the virtual DOM
+    let btnRemove = document.createElement('button'); //pulling the HTML button into JS and giving it a variable
+    btnRemove.className = 'btn btn-success'; //giving the variable a class name
+    btnRemove.id = id; //and an id, so that each button has it's own
+    btnRemove.innerHTML = 'Adios libro'; //what the button says
+    btnRemove.addEventListener ('click', () => { //the button's function
+        console.log(`Removing row with id: item-${id}`); //removed the row with selected id
+        let elementToDelete = document.getElementById(`item-${id}`); //puts that row into a variable
+        elementToDelete.parentNode.removeChild(elementToDelete);
+    });
+    return btnRemove;
+};
+
+
+
 const card = document.getElementById('card');
 card.style.backgroundColor = 'lightBlue';
 card.style.maxWidth = '600px';
 card.style.border = '3px groove darkGray';
 card.style.boxShadow = '2px 2px 2px grey';
 
-
-
-let id = 0;
-
-document.getElementById('add').addEventListener('click', () => {
-    let finishedDate = new Date();  //creates a new date, defaults to current day
-    let myTable = document.getElementById('table'); //brings the table over and creates a new variable
-    let row = myTable.insertRow(1); //header row is at position 0, so first inserted row needs to be 1
-    row.setAttribute('id', `item-${id}`); //I'M NOT SURE WHAT THIS DOES//
-    row.insertCell(0).innerHTML = document.getElementById('new-title').value; //inserting in the first column (0) the innerHTML value from 'new-title'
-    row.insertCell(1).innerHTML = document.getElementById('new-author').value //inserting in the second column (1) the innerHTML value from 'new-author'
-    row.insertCell(2).innerHTML = /*Read? */
-    row.insertCell(3).innerHTML = document.getElementById('new-rating').value; //inserting in the fourth column (3) the innerHTML value from 'new-rating'
-    row.insertCell(4).innerHTML = document.getElementById('new-finish-date').value;
-
-    document.getElementById('new-title').value = ''; //resets field to empty
-    document.getElementById('new-author').value = ''; //resets field to empty
-    document.getElementById('new-rating').value = ''; //resets field to empty
-    document.getElementById('new-finish-date').value = ''; //resets date field
-
-    let actions = row.insertRow(5);
-    actions.appendChild(createDeleteButton(id++)); //creating a delete button with a function (below)
-    document.getElementById('new-task').value = ''; //setting 'new-task' input back to clear for user experience
-})
-
-function createDeleteButton(id) {
-    let btn = document.createElement('button');
-    btn.className = 'btn btn-primary';
-    btn.id = id;
-    btn.innerHTML = 'Remove';
-    btn.onclick = () => {
-        console.log(`Remvoing row with id: item-${id}`);
-        let elementToDelete = document.getElementById(`item-${id}`);
-        elementToDelete.parentNode.removeChild(elementToDelete);
-    };
-}
+const rating = document.getElementById('rating');
+rating.style.maxWidth = '300 px';
 
 
 
@@ -47,20 +52,20 @@ function createDeleteButton(id) {
 
 
 
+// function reviewStars(num) {
+//     if (num >= 1 && num <= 5) {
 
 
 
-// function createDeleteButton(id) {       //delete button function -- creating a new element (delete button)
-//     let btn = document.createElement('button');  //creating a new button
-//     btn.className = 'btn btn-primary'; //button's class name
-//     btn.id = id;  //button's id
-//     btn.innerHTML = 'Delete'; //what the button says
-//     btn.onclick = () => {  //on the click the button with delete the row 
-//         console.log(`Deleting row with id: item-${id}`);
-//         let elementToDelete = document.getElementById(`item-${id}`);
-//         elementToDelete.parentNode.removeChild(elementToDelete);
-//     };
-//     return btn;
+
+
+        
+//     } else {
+//         return 'Try again please.'
+//     }
+// }
+// console.log(reviewStars);
+
 
 
 
