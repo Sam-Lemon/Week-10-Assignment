d1c14c7ce48fb27594290a9b4250c2307fe64b37
Sam-Lemon <theatrewhisper@gmail.com>
2024-05-11 01:13:27 -0700

    trying some stuff with an edit button

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 let id = 0;
 
+
 document.getElementById('add').addEventListener('click', () => {
     let myTable = document.getElementById('table'); //brings the table over and creates a new variable
     let row = myTable.insertRow(1); //header row is at position 0, so first inserted row needs to be 1
+    
     row.setAttribute('id', `item-${id}`); //this gives each row it's own id
     row.insertCell(0).innerHTML = document.getElementById('new-title').value; //inserting the user inputted data into the first cell of the row
     row.insertCell(1).innerHTML = document.getElementById('new-author').value //inserting the user inputted data into the second cell of the row
     row.insertCell(2).innerHTML = document.getElementById('book-notes').value //inserting the user inputted data into the third cell of the row
     row.insertCell(3).innerHTML = document.getElementById('new-finish-date').value; //inserting the user inputted date into the fourth cell of the row
+    
     let remove = row.insertCell(4); //puts the button created by the function below into the fifth cell of the row
     remove.appendChild(createDeleteButton(id++)); //attaching the function to create a delete button to the remove variable (which is inserted into it's appropriate cell)
     let edit = row.insertCell(5);
     edit.appendChild(createEditButton(id++));
-
-
 
     document.getElementById('new-title').value = ''; //resets field to empty
     document.getElementById('new-author').value = ''; //resets field to empty
@@ -21,11 +22,13 @@ document.getElementById('add').addEventListener('click', () => {
     document.getElementById('new-finish-date').value = ''; //resets date field
 });
 
+
+
 function createDeleteButton(id) { //creating a button in the virtual DOM
     let btnRemove = document.createElement('button'); //pulling the HTML button into JS and giving it a variable
-    btnRemove.className = 'btn btn-light'; //using bootstrap to style the button
+    btnRemove.className = 'btn btn-secondary'; //using bootstrap to style the button
     btnRemove.id = id; //and an id, so that each button has it's own
-    btnRemove.innerHTML = 'Adios libro'; //what the button says
+    btnRemove.innerHTML = 'Remove'; //what the button says
     btnRemove.addEventListener ('click', () => { //the button's function
         console.log(`Removing row with id: item-${id}`); //removed the row with selected id
         let elementToDelete = document.getElementById(`item-${id}`); //puts that row into a variable
@@ -33,39 +36,6 @@ function createDeleteButton(id) { //creating a button in the virtual DOM
     });
     return btnRemove;
 };
-
-// edit button on click 
-// create new card
-// auto populate current values for fields
-// user changes what they want then clicks Submit
-// submit adds the changed values back into the table
-
-
-function createEditButton(id) {
-    let btnEdit = document.createElement('button'); //creates edit button
-    btnEdit.className = 'btn btn-light'; //uses bootstrap to style it
-    btnEdit.id = id; //gives it an id 
-    btnEdit.innerHTML = 'Edit'; //what the button itself says
-
-    btnEdit.addEventListener('click', () => { //adding an event listener that, on a click, will perform the below function
-        console.log(`Editing row with id: item-${id}`);
-        let elementToEdit = document.getElementById(`item-${id}`); //puts the row we want to edit into a variable
-
-        //take the information from the row and put it into the form
-        let editForm = document.getElementById('card-body');
-        editForm.appendChild(elementToEdit);
-
-
-     });
-     return btnEdit;
-
-    };
-
-
-
-
-
-
 
 const card = document.getElementById('card');
 card.style.backgroundColor = 'lightBlue';
@@ -84,6 +54,58 @@ body.style.fontFamily = 'serif';
 
 
 
+
+
+/*//////////////////////////////////////////////////////////////////////
+I would really like to make this edit button auto-populate the current 
+information into the form and then update the table with the user edited 
+information. However, I'm getting stuck at how to extrapolate the values 
+from the cells of the row. A couple of things I tried are commented out
+below.
+//////////////////////////////////////////////////////////////////////*/
+
+
+function createEditButton(id) {
+    let btnEdit = document.createElement('button'); //creates edit button
+    btnEdit.className = 'btn btn-secondary'; //uses bootstrap to style it
+    btnEdit.id = id; //gives it an id 
+    btnEdit.innerHTML = 'Edit'; //what the button itself says
+
+    btnEdit.addEventListener('click', () => { //adding an event listener that, on a click, will perform the below function
+        console.log(`Editing row with id: item-${id}`);
+
+        
+
+        
+        let elementToEdit = document.getElementById(`item=${id}`);
+
+
+
+            // This version pops error: uncaught typeerror: cannot read properties of null (reading 'getElementsByTagName') at HTMLButtonElement.<anonymous> (index.js:76:31)
+        // let editedRow = document.getElementById(`item-${id}`);
+        // let cells = editedRow.getElementsByTagName('new-title');
+        // alert(Cells[0].innerText);
+
+     });
+     return btnEdit;
+
+    };
+
+
+
+// function createUpdateButton(id) {
+//     let btnUpdate = document.createElement('button');
+//     btnUpdate.className = 'btn btn-secondary';
+//     btnUpdate.innerHTML = 'Update';
+
+//     btnUpdate.addEventListener('click', () => {
+//         row.insertCell(0).innerHTML = document.getElementById('new-title').value; //inserting the user inputted data into the first cell of the row
+//         row.insertCell(1).innerHTML = document.getElementById('new-author').value //inserting the user inputted data into the second cell of the row
+//         row.insertCell(2).innerHTML = document.getElementById('book-notes').value //inserting the user inputted data into the third cell of the row
+//         row.insertCell(3).innerHTML = document.getElementById('new-finish-date').value; //inserting the user inputted date into the fourth cell of the row
+
+//     })
+// }
 
 
 
